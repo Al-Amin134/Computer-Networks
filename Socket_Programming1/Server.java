f79cfd5bc6bfa062388294eca96632cd7d9eacc9
Md.Al-Amin Babu <mdalaminbabu.210@gmail.com>
2025-07-15 01:35:27 +0600

    Update Server.java

--- Socket_Programming1/Server.java
+++ Socket_Programming1/Server.java
@@ -5,44 +5,48 @@ import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Server {
-    public static void main(String[] args) throws Exception{
+public class sever {
+    public static void main(String[] args)throws Exception {
         ServerSocket serverSocket = new ServerSocket(7000);
-        System.out.println("The server runs in the port 7000\n");
+        System.out.println("Server is running on the port 1000\n");
 
         Socket socket2 = serverSocket.accept();
         DataOutputStream dos2 = new DataOutputStream(socket2.getOutputStream());
         DataInputStream dis2 = new DataInputStream(socket2.getInputStream());
-
         while(true){
-            String data = dis2.readUTF();
-            if(data.equalsIgnoreCase("HI")){
-                StringBuilder s = new StringBuilder();
-                for(int i=1;i<=9;i++){
-                 s.append("Hello").append(i).append("\n");
-                }
-                dos2.writeUTF(s.toString());
+            String s;
+            s = dis2.readUTF();
+            System.out.println("The message from client is :"+s);
+            if(s.equals("HI")){
+               for(int i=0;i<8;i++){
+             dos2.writeUTF(s);
+               }
             }
-            else if(data.equalsIgnoreCase("Hello")){
+            else if(s.equals("HELLO")){
                 dos2.writeUTF("Hello Client\n");
             }
-            else if(data.equalsIgnoreCase("Time")){
-                String time = getTime();
-                dos2.writeUTF(time);
+            else if(s.equals("DATE")){
+                String date = getdate();
+                dos2.writeUTF(date);
             }
-            else if(data.equalsIgnoreCase("QUIT")){
+           else if(s.equals("QUIT")){
                 dos2.writeUTF("BYE");
                 break;
             }
             else{
-                dos2.writeUTF("Invalid Input\n");
+                dos2.writeUTF("INVALID");
+               
             }
         }
+        dos2.close();
+        dis2.close();
+        socket2.close();
     }
-
-    public static String getTime(){
+    private static String getdate()throws Exception{
         SimpleDateFormat f = new SimpleDateFormat("HH:MM:SS");
         Date date = new Date();
-        return(f.format(date));
+        return f.format(date);
+
+
     }
 }
